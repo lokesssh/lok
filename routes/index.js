993367174611c4ff80189783ecc1e7ed36998cf5
lokesssh <lokeshmugala@gmail.com>
2017-06-22 17:45:29 +0530

    mgfj

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,25 @@ router.get('/WEL', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/find',function(req,res,next){
-docs.find({},function(err,docs){
-  if(err) console.log(err);
-  else res.json(docs);
-})
+router.get('/find',function(req,res,next) {
+    docs.find({}, function (err, docs) {
+        if (err) console.log(err);
+        else res.json(docs);
+    })
+});
+    router.get('/in',function(req,res,next){
+        docs.update({name:"lokesh"},{$set:{age:"20"}},function(err,docs){
+            if(err) console.log(err);
+            else res.json(docs);
+        })
 
+});
+    route.get('/ini',function(req,res,next)
+{
+    docs.update({name: "lokesh"},{$set:{batch:"2018"}},function(err, docs){
+        if (err) console.log(err);
+        else res.json(docs);
+    })
 });
 
 module.exports = router;
